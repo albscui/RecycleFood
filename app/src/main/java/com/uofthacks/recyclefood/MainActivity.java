c211bdb81ec108c626ef0887b821881127de57a2
Daniel Chan <iam@danielchan.me>
2016-01-23 03:56:45 -0500

    Firebase Auth
    Google O2Auth

--- app/src/main/java/com/uofthacks/recyclefood/MainActivity.java
+++ app/src/main/java/com/uofthacks/recyclefood/MainActivity.java
@@ -9,15 +9,32 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Firebase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
+
+        mRef = new Firebase("https://luminous-fire-5527.firebaseio.com/");
+        mRef.authWithOAuthToken("google", "<OAuth Token>", new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                // the Google user is now authenticated with your Firebase app
+            }
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                // there was an error
+            }
+        });
 
     }
 
