c11e0d9a5cfe6a154bb8e17c1fb1d68c7bf5fd38
Daniel Chan <iam@danielchan.me>
2016-01-23 04:30:50 -0500

    More Facebook Login

--- app/src/main/java/com/uofthacks/View/LoginActivity.java
+++ app/src/main/java/com/uofthacks/View/LoginActivity.java
@@ -1,15 +1,121 @@
 package com.uofthacks.View;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.uofthacks.recyclefood.MainActivity;
 import com.uofthacks.recyclefood.R;
 
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
+
+
+
+    /* The login button for Facebook */
+    private LoginButton mFacebookLoginButton;
+    /* The callback manager for Facebook */
+    private CallbackManager mFacebookCallbackManager;
+    /* Used to track user logging in/out off Facebook */
+    private AccessTokenTracker mFacebookAccessTokenTracker;
+
+
+    /* TextView that is used to display information about the logged in user */
+    private TextView mLoggedInStatusTextView;
+
+    /* A dialog that is presented until the Firebase authentication finished. */
+    private ProgressDialog mAuthProgressDialog;
+
+    /* A reference to the Firebase */
+    private Firebase mFirebaseRef;
+
+    /* Data from the authenticated user */
+    private AuthData mAuthData;
+
+    /* Listener for Firebase session changes */
+    private Firebase.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFacebookCallbackManager = CallbackManager.Factory.create();
+        mFacebookLoginButton = (LoginButton) findViewById(R.id.login_button);
+        mFacebookAccessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                Log.i(TAG, "Facebook.AccessTokenTracker.OnCurrentAccessTokenChanged");
+                LoginActivity.this.onFacebookAccessTokenChange(currentAccessToken);
+            }
+        };
+    }
+
+    private void onFacebookAccessTokenChange(AccessToken token) {
+        if (token != null) {
+            mAuthProgressDialog.show();
+            mFirebaseRef.authWithOAuthToken("facebook", token.getToken(), new Firebase.AuthResultHandler("facebook"));
+        } else {
+            // Logged out of Facebook and currently authenticated with Firebase using Facebook, so do a logout
+            if (this.mAuthData != null && this.mAuthData.getProvider().equals("facebook")) {
+                mFirebaseRef.unauth();
+                setAuthenticatedUser(null);
+            }
+        }
+    }
+
+    private void authWithFirebase(final String provider, Map<String, String> options) {
+        if (options.containsKey("error")) {
+            showErrorDialog(options.get("error"));
+        } else {
+            mAuthProgressDialog.show();
+            if (provider.equals("twitter")) {
+                // if the provider is twitter, we pust pass in additional options, so use the options endpoint
+                mFirebaseRef.authWithOAuthToken(provider, options, new Firebase.AuthResultHandler(provider));
+            } else {
+                // if the provider is not twitter, we just need to pass in the oauth_token
+                mFirebaseRef.authWithOAuthToken(provider, options.get("oauth_token"), new Firebase.AuthResultHandler(provider));
+            }
+        }
+    }
+
+    private void setAuthenticatedUser(AuthData authData) {
+        if (authData != null) {
+            /* Hide all the login buttons */
+            mFacebookLoginButton.setVisibility(View.GONE);
+            mLoggedInStatusTextView.setVisibility(View.VISIBLE);
+            /* show a provider specific status text */
+            String name = null;
+            if (authData.getProvider().equals("facebook")
+                    || authData.getProvider().equals("google")
+                    || authData.getProvider().equals("twitter")) {
+                name = (String) authData.getProviderData().get("displayName");
+            } else if (authData.getProvider().equals("anonymous")
+                    || authData.getProvider().equals("password")) {
+                name = authData.getUid();
+            } else {
+                Log.e(TAG, "Invalid provider: " + authData.getProvider());
+            }
+            if (name != null) {
+                mLoggedInStatusTextView.setText("Logged in as " + name + " (" + authData.getProvider() + ")");
+            }
+        } else {
+            /* No authenticated user show all the login buttons */
+            mFacebookLoginButton.setVisibility(View.VISIBLE);
+            mLoggedInStatusTextView.setVisibility(View.GONE);
+        }
+        this.mAuthData = authData;
+        /* invalidate options menu to hide/show the logout button */
+        supportInvalidateOptionsMenu();
     }
 }
