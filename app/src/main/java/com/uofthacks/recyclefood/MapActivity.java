9366a8fa9ae40fcb3d582ca7439c4ab4ae758741
albscui <albrtcui@gmail.com>
2016-01-23 16:57:07 -0500

    refactor MapActivity and its xml, change the getMapAsync method

--- app/src/main/java/com/uofthacks/recyclefood/MapActivity.java
+++ app/src/main/java/com/uofthacks/recyclefood/MapActivity.java
@@ -1,5 +1,6 @@
 package com.uofthacks.recyclefood;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Activity;
@@ -9,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,7 +19,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends Activity {
 
     GoogleMap map;
-    
+    Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,39 +28,49 @@ public class MapActivity extends Activity {
 
 
         try {
-            map = ((MapFragment) getFragmentManager().findFragmentById(
-                    R.id.fragment1)).getMapAsync(map);
+            ( (MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map_fragment)).getMapAsync(new OnMapReadyCallback() {
 
-            MarkerOptions k = new MarkerOptions()
-                    .position(new LatLng(28.573208, 77.206383))
-                    .title("Current Location")
-                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
-                    .draggable(true).snippet("Your current location");
-            map.addMarker(k);
-            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            map.setMyLocationEnabled(true);
-            map.getUiSettings().setZoomControlsEnabled(false);
-            map.getUiSettings().setZoomGesturesEnabled(false);
-            map.getUiSettings().setCompassEnabled(true);
-            map.getUiSettings().setMyLocationButtonEnabled(true);
-            map.getUiSettings().setRotateGesturesEnabled(true);
-            map.getUiSettings().setRotateGesturesEnabled(true);
-            map.getUiSettings().setTiltGesturesEnabled(true);
 
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(28.573208, 77.206383)).zoom(12).build();
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    map = googleMap;
+                    MarkerOptions k = new MarkerOptions()
+                            .position(new LatLng(28.573208, 77.206383))
+                            .title("Current Location")
+                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+                            .draggable(true).snippet("Your current location");
+                    map.addMarker(k);
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    if (ActivityCompat.checkSelfPermission(context,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    map.setMyLocationEnabled(true);
+                    map.getUiSettings().setZoomControlsEnabled(false);
+                    map.getUiSettings().setZoomGesturesEnabled(false);
+                    map.getUiSettings().setCompassEnabled(true);
+                    map.getUiSettings().setMyLocationButtonEnabled(true);
+                    map.getUiSettings().setRotateGesturesEnabled(true);
+                    map.getUiSettings().setRotateGesturesEnabled(true);
+                    map.getUiSettings().setTiltGesturesEnabled(true);
 
-            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(new LatLng(28.573208, 77.206383)).zoom(12).build();
+
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                }
+            });
 
         } catch (Exception e){
             e.printStackTrace();
