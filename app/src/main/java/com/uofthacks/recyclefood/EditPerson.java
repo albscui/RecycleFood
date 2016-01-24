cbc9eef2d040e334185e9b9687789ef787b2d04c
patrickhsu1995 <patrick.hsu@mail.utoronto.ca>
2016-01-24 02:06:00 -0500

    Removed Picture1.png

--- app/src/main/java/com/uofthacks/recyclefood/EditPerson.java
+++ app/src/main/java/com/uofthacks/recyclefood/EditPerson.java
@@ -5,10 +5,10 @@ import android.app.Activity;
 
 public class EditPerson extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_person);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_edit_person);
+//    }
 
 }
