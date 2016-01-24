b20edbae2d55e610c39b9b02f9828492465a84fb
Truman Wang <truman.wang@mail.utoronto.ca>
2016-01-23 19:10:34 -0500

    added Edit Person Activity and a test button

---
+++ app/src/main/java/com/uofthacks/recyclefood/EditPerson.java
@@ -0,0 +1,14 @@
+package com.uofthacks.recyclefood;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class EditPerson extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_person);
+    }
+
+}
